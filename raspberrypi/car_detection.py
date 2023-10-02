6dd3cb92ca1dfe6202bc926f7629b72b01bcbb36
Shreyas Nair <shreyasnair934@gmail.com>
2023-10-02 13:20:01 +1100

    Added code that communicates with Allan's Raspberry Pi and checks whether parking lot is full

--- raspberrypi/car_detection.py
+++ raspberrypi/car_detection.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import paho.mqtt.client as mqtt
 from dotenv import load_dotenv, dotenv_values
 import cv2
 import numpy as np
@@ -19,6 +20,27 @@ client = MongoClient(uri, server_api=ServerApi("1"))
 # Connect to the MongoDB database and collection within the database
 db = client[f"{db_name}"]
 image_collection = db["car_images"]
+
+distances = {
+    "parking_lot/sensors/ultrasonic1": 0,
+    "parking_lot/sensors/ultrasonic2": 0
+}
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe("parking_lot/sensors/ultrasonic1")
+    client.subscribe("parking_lot/sensors/ultrasonic2")
+
+def on_message(client, userdata, msg):
+    print(f"{msg.topic}: {msg.payload.decode()}")
+
+    # Update the respective sensor's distance
+    distances[msg.topic] = float(msg.payload.decode())
+
+   
+    if distances["parking_lot/sensors/ultrasonic1"] < 100 and distances["parking_lot/sensors/ultrasonic2"] < 100:
+        print("Parking bay full")
+        time.sleep(10)
 
 
 camera = cv2.VideoCapture(0)
@@ -60,6 +82,17 @@ while True:
         last_captured_time = current_time
 
     cv2.imshow("Car Detection", frame)
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("broker.hivemq.com", 1883, 60)
+
+    client.loop_start()
+    time.sleep(5)  # Loop for 10 seconds
+    client.loop_stop()
+    time.sleep(5)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
